f020051db36fbe288b9bd5ef10a011886e3455e5
yuudinwah <wahyuwahyudin443@gmail.com>
2025-01-12 23:18:04 +0700

    fix fonts swap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import HeaderComponent from "@/components/Header";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: 'swap'
 });
 
 const geistMono = Geist_Mono({
